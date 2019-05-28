171497c15b097829b3970433cee2759a4368a7ae
Marnelas <marnelasbenito@gmail.com>
2019-05-28 11:52:44 +0200

    tittle changed

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ hbs.registerHelper('ifUndefined', (value, options) => {
 
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Enjoy Your Records';
 
 
 // Enable authentication using session + passport
