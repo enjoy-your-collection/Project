8c6252fb426f4e513d1dc1574ddd72024c7b5b55
Marnelas <marnelasbenito@gmail.com>
2019-05-28 19:01:49 +0200

    some bugs mended

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,29 +3,53 @@ const router = express.Router();
 const axios = require("axios")
 const Review = require("../models/review")
 const Artist = require("../models/artist")
+const Discography = require("../models/discography")
     /* GET home page */
 router.get('/', (req, res, next) => {
     res.render('index');
 });
 
 router.get("/artist/:artist_id/release", (req, res, next) => {
-    axios
-        .get(`https://api.discogs.com/artists/${req.params.artist_id}/releases`)
-        .then(response => {
-            res.render("release", response.data);
-        })
-        .catch(err => console.log("hubo un error", err));
+    Discography.findOne({ idArtist: req.params.artist_id })
+        .then(found => {
+            if (!found) {
+                newRel();
+                return;
+            }
+            console.log("este ya estaba");
+            res.render("discography", found);
+        });
 
+    const newRel = () => {
+        axios
+            .get(`https://api.discogs.com/artists/${req.params.artist_id}/releases`)
+            .then(response => {
+                console.log(response.data.releases)
+
+                const releases = response.data
+                console.log(releases)
+                const artistID = req.params.artist_id
+                const newDisco = new Discography({
+                    releases,
+                    idArtist: artistID
+                });
+                newDisco.save()
+                    .then((created) => {
+                        res.render("discography", created);
+
+
+                    })
+            })
+            .catch(err => res.render("discography", { msg: "ese album no esta disponible", }));
+
+
+    }
 })
 
 
+
 router.get('/artist/albums/:release_id', (req, res, next) => {
-    console.log("esto es ", req.params.release_id)
-        // let reviews = []
-        // Review.find({})
-        //     .then(elm => reviews = [...elm])
-        //     .catch(err => console.log("error de find", err))
-        // console.log(reviews)
+
     axios
         .get(`https://api.discogs.com/masters/${req.params.release_id}`)
         .then(response => {
@@ -33,7 +57,7 @@ router.get('/artist/albums/:release_id', (req, res, next) => {
                 res.render("albums", { album: response.data, review: elm })
             );
         })
-        .catch(err => res.render("release", { msg: "ese album no esta disponible", }));
+        .catch(err => res.render("discography", { msg: "ese album no esta disponible", }));
 
 
 })
@@ -48,12 +72,10 @@ router.get("/artist/:artist_id", (req, res, next) => {
                 return
 
             }
-            console.log("este ya estaba")
             res.render("artist-detail", found)
         });
 
     const newArt = () => {
-        console.log("este es nuevo")
         axios.get(`https://api.discogs.com/artists/${req.params.artist_id}`)
             .then(response => {
                 const { name, profile, members, id } = response.data
@@ -65,33 +87,28 @@ router.get("/artist/:artist_id", (req, res, next) => {
                 });
                 newArtist.save()
                     .then((created) => {
-                        console.log("guardado", created)
                         res.render("artist-detail", created);
 
 
                     })
 
             })
-            .catch(err => console.log("hubo un error", err));
+            .catch(err => res.render("artist-detail", { msg: "no se pudo encontrar ese artista" }))
     }
+})
 
-});
 router.post("/artists", (req, res, post) => {
-    axios
-        .get(
+    axios.get(
             `https://api.discogs.com/database/search?q=${req.body.artist}&key=${process.env.discogsKey}&secret=${process.env.discogsSecret}`
         )
         .then(results => {
-            console.log(results.data);
             res.render("search", { results: results.data });
         })
         .catch(err => console.log(err));
 })
 router.post("/review/:album_id", (req, res, next) => {
     const { title, rate, description } = req.body;
-    console.log("el ratio es", req.body.rate)
     const id = req.params.album_id
-    console.log("album id", id)
     const newReview = new Review({
         title,
         rating: rate,
@@ -100,7 +117,6 @@ router.post("/review/:album_id", (req, res, next) => {
     });
     newReview.save()
         .then(x => {
-            console.log("guardado", x)
             res.redirect(`/artist/albums/${id}`)
         })
         .catch(error => console.log(error));
